bcb55056f74cc39ec678480169eb2577a4ecfa3e
usama <usamayaar163@gmail.com>
2024-11-27 00:21:39 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Main from "./components/Main/Main";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return <>
+  <Sidebar/>
+  <Main/>
+  </>;
+}
+
+export default App;
